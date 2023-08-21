2d98bf73aa088df2cae94e15b80904ddff8a1617
sebafudi <sebastianfudalej@gmail.com>
2023-08-21 23:03:18 +0200

    feat: add compatibility with WLED

--- internal/connection/send.go
+++ internal/connection/send.go
@@ -7,6 +7,7 @@ import (
 
 func SendUdpPacket(conn net.Conn, ledArray [][3]byte) {
 	var bytesToSend []byte
+	bytesToSend = append(bytesToSend, 0x02, 0x01)
 	for i := 0; i < 97; i++ {
 		for j := 0; j < 3; j++ {
 			bytesToSend = append(bytesToSend, ledArray[i][j])
